b3a2f2bdcda7133b870e5bc81ff8fa5d2165db21
tshmieldev <wojtekrok88@gmail.com>
2026-06-12 16:50:59 +0200

    repro setup

--- App.tsx
+++ App.tsx
@@ -5,19 +5,22 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { NewAppScreen } from "@react-native/new-app-screen";
+import { StatusBar, StyleSheet, useColorScheme, View } from "react-native";
+import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { scheduleOnUI } from "react-native-worklets";
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
+
+  scheduleOnUI(() => {
+    console.log(2 + 2);
+  });
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
       <AppContent />
     </SafeAreaProvider>
   );
@@ -28,10 +31,7 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <NewAppScreen templateFileName="App.tsx" safeAreaInsets={safeAreaInsets} />
     </View>
   );
 }
